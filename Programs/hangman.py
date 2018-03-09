51f76970df1525fa9632fb4f0ca7b627157a1811
gjergjk71 <gjergjk71@gmail.com>
2018-03-09 17:33:53 +0100

    Added new features/Fixed some bugs

--- Programs/hangman.py
+++ Programs/hangman.py
@@ -1,15 +1,24 @@
 """
 
-Version 1.9
+Version 1.9.1
 
 Changes:
-    -Corrected typo/Deleted unnessecary comments
-    -The game continues even after you type all the letters or the word.
-    -After you guessed the word it asks you if you want to continue
-    -Spaces are not counted so " b" is same as "b"
-    -Fixed a bug when the user types the whole word but
-        doesn't end!
-    -The first letter of the word is always uppercase
+    -Changed the "You Lost!" message from normal text to ASCII Art.
+    -Fixed a bug where the user could type the same letter multiple times and win!
+    -When you lose the word you had to guess is printed.
+    -Prints the letters/words you've already tried
+    -Doesn't print how many chances you have left anymore.
+
+To-Do:
+    -Redesign the code... what was I thinking when using so much global
+       variable wtf -_-
+    -Find a way to use this piece of code right after the user input is taken
+    "if guess_Word not in tried_Letters:    
+       tried_Letters.append(guess_Word)"
+    -Fix this output
+         You guessed a letter!
+         You've already guessed that letter!
+    
 """
 
 import random, sys
@@ -22,6 +31,14 @@ hangman_art = """
 /_/ /_//_/  |_|/_/ |_/ \____//_/  /_//_/  |_|/_/ |_/   
 
  """
+
+gameOver_art = """
+ __   __            _              _   _ 
+ \ \ / /__  _   _  | |    ___  ___| |_| |
+  \ V / _ \| | | | | |   / _ \/ __| __| |
+   | | (_) | |_| | | |__| (_) \__ \ |_|_|
+   |_|\___/ \__,_| |_____\___/|___/\__(_)
+"""
 
 stages  = ['''
  =========''', '''
@@ -105,22 +122,23 @@ def generateList():
     global letters_List
     global guessed_number
     #Testing purposes
-    print(random_word)
+    #print(random_word)
     guessed_number = 1
     randomMinusNumber = len(random_word) - guessed_number
     letters_Left = list(random_word)[0] + "_" * randomMinusNumber
     letters = letters_Left
     letters_List = list(letters)
-    indexCheck = 1
 
 def restart():
     print("Restarting...")
     global chances
     global guess_Word
+    global guessed_Letters
     guess_Word = ""
     pickWord()
     chances = 10
-    
+    indexCheck = 1
+    guessed_Letters = []
 
 def askPlay():
     global play
@@ -137,11 +155,16 @@ def askPlay():
             print("Chose a valid option!")
         break
 
+def guessed_all_letters():
+    if guessed_number == len(random_word):
+        print(str(letters_List))
+        print("You guessed all the letters!")
+        askPlay()
 
+tried_Letters = []
 chances = 10
 
 print(hangman_art)
-print("---Hangman Game---")
 print("--You have 10 guesses!--")
 
 
@@ -163,21 +186,28 @@ while playing:
                 break
             except ValueError:
                 print("Please don't enter integers or floating numbers!")
+        if guess_Word not in tried_Letters:    
+            tried_Letters.append(guess_Word)
         #Testing purposes print(len(guess_Word))
+        print("Words/Letters you've already tried: ")
+        print(*tried_Letters, sep=", ")
         if len(guess_Word) == 1:
             indexCheck = 0
             global indexCheck
             global chances
-            #print(indexCheck)
+            print("(Testing) The word is :" + random_word)
             while indexCheck <= len(random_word) - 1:
                 if guess_Word in list(random_word[indexCheck]):
-                    print("Guess word" + guess_Word)
-                    print("Index Check " + str(indexCheck))
                     global guessed_number
                     global letters_List
-                    guessed_number += 1
+                    global tried_Letters
+                    #Testing purposes
+                    #print("Guess word- " + guess_Word)
+                    #print("Index Check " + str(indexCheck))
+                    if guess_Word not in tried_Letters:
+                        guessed_number += 1
                     letters_List[indexCheck] = guess_Word
-                    #print(letters_List)
+                    print(letters_List)
                     if guessed_number != len(random_word):
                         print("You guessed a letter!")
                     if indexCheck == len(random_word) - 1:
@@ -185,14 +215,14 @@ while playing:
                 indexCheck += 1
             if guess_Word in letters_List:
                 print("You've already guessed that letter!")
-        
-                
+              
         if guess_Word != random_word and guess_Word not in list(random_word):
             chances -= 1
             stages_index += 1
         if stages_index == 10 and guessed_number != len(random_word):
-            print("You lost!")
-            break
+            print("The word was: " + random_word)
+            print(gameOver_art)
+            sys.exit()
         #Check if the user guessed all the letters       
         if guessed_number == len(random_word):
             print(str(letters_List))
@@ -200,12 +230,8 @@ while playing:
             askPlay()
             
         #Check if the user guessed the whole word
-        if guess_Word == random_word.lower():
-            print(str(letters_List))
-            print("You guessed the word!")
-            askPlay()
-
-        #Print chances left if user
-        elif guess_Word != random_word:
-            print("You have " + str(chances) + " chances left!")
+            if guess_Word == random_word.lower():
+                print(str(letters_List))
+                print("You guessed the word!")
+                askPlay()
     

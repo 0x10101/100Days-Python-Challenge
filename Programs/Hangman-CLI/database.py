29f651dfaae71021ea4285e802667628a278ebb2
gjergjk71 <gjergjk71@gmail.com>
2018-03-20 20:45:23 +0100

    Created addScore(currentScore,addScore) function

--- Programs/Hangman-CLI/database.py
+++ Programs/Hangman-CLI/database.py
@@ -18,17 +18,27 @@ def insertAccount(user,passw):
         try:
                 db = sqlite3.connect("database.db")
                 cursor = db.cursor()
-                        cursor.execute("""INSERT INTO accounts(username,password)
-                                        VALUES(?,?)""", (user,passw))
+                cursor.execute("""INSERT INTO accounts(username,password)
+                                VALUES(?,?)""", (user,passw))
                 db.commit()
                 for account in cursor.execute("SELECT * from accounts"):
                         for value in account:
                                 print(value)
                 db.close()
         except sqlite3.IntegrityError:
-		userTaken = True
-		print("Username is already taken!")
-	if not userTaken:
-		print("Account successfully registered. Please log in!")
-	return userTaken
+                userTaken = True
+                print("Username is already taken!")
+        if not userTaken:
+                print("Account successfully registered. Please log in!")
+        return userTaken
+
+def addScore(currentScore,addScore):
+        db = sqlite3.connect("database.db")
+        cursor = db.cursor()
+        cursor.execute("""
+            UPDATE accounts SET score = ?
+            WHERE username = ? """, (currentScore,addScore))
+        db.commit()
+        db.close()
+                
 

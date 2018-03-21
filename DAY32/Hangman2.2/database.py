a3e372e50fa8ba86a1fc8f3a14e575db4e83e377
gjergjk71 <gjergjk71@gmail.com>
2018-03-21 20:26:46 +0100

    Created showScoreboard() function

---
+++ DAY32/Hangman2.2/database.py
@@ -0,0 +1,52 @@
+#Created showScoreboard() function
+
+import sqlite3
+
+
+def createDatabase():		
+        db = sqlite3.connect("database.db")
+        cursor = db.cursor()
+        cursor.execute("""
+        CREATE TABLE IF NOT EXISTS accounts (
+                ID	INTEGER,
+                username	TEXT	UNIQUE,
+                password	TEXT,
+                score	INTEGER DEFAULT 0,
+                PRIMARY KEY(`ID`)) """)
+        db.commit()
+
+def insertAccount(user,passw):
+        userTaken = False
+        try:
+                db = sqlite3.connect("database.db")
+                cursor = db.cursor()
+                cursor.execute("""INSERT INTO accounts(username,password)
+                                VALUES(?,?)""", (user,passw))
+                db.commit()
+                for account in cursor.execute("SELECT * from accounts"):
+                        for value in account:
+                                print(value)
+                db.close()
+        except sqlite3.IntegrityError:
+                userTaken = True
+                print("Username is already taken!")
+        if not userTaken:
+                print("Account successfully registered. Please log in!")
+        return userTaken
+
+def addScore(currentScore,addScore):
+        db = sqlite3.connect("database.db")
+        cursor = db.cursor()
+        cursor.execute("""
+            UPDATE accounts SET score = ?
+            WHERE username = ? """, (currentScore,addScore))
+        db.commit()
+        db.close()
+
+def showScoreboard():
+        db = sqlite3.connect("database.db")
+        cursor = db.cursor()
+        for account in cursor.execute("SELECT * from accounts"):
+                print("{} Score --> {}".format(account[1],account[3]))
+        db.commit()
+        db.close()

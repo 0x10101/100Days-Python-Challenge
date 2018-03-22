e6082b86fe2e7e2a7d277537e8ac86ea16b99332
gjergjk71 <gjergjk71@gmail.com>
2018-03-22 21:56:10 +0100

    Created checkLevel(..) to check what lv user is

--- Programs/Hangman-CLI/level_system.py
+++ Programs/Hangman-CLI/level_system.py
@@ -8,3 +8,20 @@ def generateLevel():
 		plus += 150
 	return level
 
+def checkLevel(score):
+	levels = generateLevel()
+	print(levels)
+	currentLevel = 1
+	for key, value in levels:
+		#print("KEY: {} \n VALUE {}".format(key,value))
+		if value < score:
+			currentLevel = key
+	return currentLevel
+	
+	#db = sqlite3.connect("database.db")
+	
+	#cursor = db.cursor()
+
+	#db.close()
+
+#checkLevel(2500)

8d63c78da65c0dc4aa2d3d048fc4fc72b93c7c46
gjergjk71 <gjergjk71@gmail.com>
2018-04-01 14:28:09 +0200

    Designed Sign in

--- Programs/School-Management/main.py
+++ Programs/School-Management/main.py
@@ -3,8 +3,30 @@ from tkinter import ttk
 
 root = tk.Tk()
 root.title("School Management")
-root.geometry("900x700")
+root.geometry("{}x{}".format(900,700))
 
+
+root.grid_columnconfigure(1, weight=1)
+root.grid_columnconfigure(1, weight=1)
+
+l1 = tk.Label(root,text="Sign in",font=("",70))
+l1.place(x=50,y=50)
+
+l2 = tk.Label(root,text="Username: ",font=("",30))
+l2.place(x=130,y=180)
+
+#e2_content = 
+e2 = tk.Entry(root)
+e2.place(x=360,y=180,width=300,height=55)
+
+l3 = tk.Label(root,text="Password:",font=("",30))
+l3.place(x=135,y=250)
+
+e3 = tk.Entry(root)
+e3.place(x=360,y=250,width=300,height=55)
+
+b1 = tk.Button(root,text="Login",font=("",28))
+b1.place(x=310,y=330,width=150,height=50)
 
 
 

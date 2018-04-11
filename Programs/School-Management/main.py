411add9745ab78326746f0db78157b35d28141b9
gjergjk71 <gjergjk71@gmail.com>
2018-04-11 20:48:26 +0200

    Created loginAttempt() function

--- Programs/School-Management/main.py
+++ Programs/School-Management/main.py
@@ -1,10 +1,23 @@
 import tkinter as tk
 from tkinter import ttk
+from database import Manage
+
+def loginAttempt():
+	access = False
+	dbManager = Manage("database.db") #fileLocation
+	dbManager.connect()
+	dbManager.create_table("accounts")
+	c = dbManager.conn.cursor()
+	for account in c.execute("SELECT * FROM accounts"):
+		if account[3] == e1.get() and account[4] == e2.get():
+			access = True
+			break
+	print(access)
+	return access
 
 root = tk.Tk()
 root.title("School Management")
 root.geometry("{}x{}".format(900,700))
-
 
 root.grid_columnconfigure(1, weight=1)
 root.grid_columnconfigure(1, weight=1)
@@ -16,16 +29,16 @@ l2 = tk.Label(root,text="Username: ",font=("",30))
 l2.place(x=130,y=180)
 
 #e2_content = 
-e2 = tk.Entry(root)
-e2.place(x=360,y=180,width=300,height=55)
+e1 = tk.Entry(root)
+e1.place(x=360,y=180,width=300,height=55)
 
 l3 = tk.Label(root,text="Password:",font=("",30))
 l3.place(x=135,y=250)
 
-e3 = tk.Entry(root)
-e3.place(x=360,y=250,width=300,height=55)
+e2 = tk.Entry(root)
+e2.place(x=360,y=250,width=300,height=55)
 
-b1 = tk.Button(root,text="Login",font=("",28))
+b1 = tk.Button(root,text="Login",font=("",28),command=loginAttempt)
 b1.place(x=310,y=330,width=150,height=50)
 
 

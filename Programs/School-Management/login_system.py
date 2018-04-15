f30c97cda7bc364e2db1667cebc72d0d54027fe5
gjergjk71 <gjergjk71@gmail.com>
2018-04-15 10:11:33 +0200

    LoginSystem().login(..) returns account dict

--- Programs/School-Management/login_system.py
+++ Programs/School-Management/login_system.py
@@ -6,11 +6,21 @@ class LoginSystem():
 	def login(self,usern,passw):
 		dbManager = db.Manage("database.db")
 		dbManager.connect()
-		account = dbManager.getTableData("accounts","username='{}' and password='{}'".format(usern,passw))
+		acc = dbManager.getTableData("accounts","username='{}' and password='{}'".format(usern,passw))
+		account = {
+				"ID":"{}".format(acc[0][0]),
+				"First Name":"{}".format(acc[0][1]),
+				"Last Name":"{}".format(acc[0][2]),
+				"Username":"{}".format(acc[0][3]),
+				"Password":"{}".format(acc[0][4]),
+				"Birthday":"{}".format(acc[0][5])
+		}
+		print(account)
+		dbManager.close()
 		if account:
-			return True
+			return account
 		else:
 			return False
 
-#access = LoginSystem().loginAttempt("gjergjk71","gjergji.123")
-#print(access)
+access = LoginSystem().login("gjergjk71","gjergji.123")
+print(access["First Name"])

29a46052df7065d70c9a2e8e440d80e98191d160
gjergjk71 <gjergjk71@gmail.com>
2018-04-15 10:46:33 +0200

    login() method doesn't need to turn the acc data to a dict anymore

--- Programs/School-Management/login_system.py
+++ Programs/School-Management/login_system.py
@@ -7,20 +7,11 @@ class LoginSystem():
 		dbManager = db.Manage("database.db")
 		dbManager.connect()
 		acc = dbManager.getTableData("accounts","username='{}' and password='{}'".format(usern,passw))
-		account = {
-				"ID":"{}".format(acc[0][0]),
-				"First Name":"{}".format(acc[0][1]),
-				"Last Name":"{}".format(acc[0][2]),
-				"Username":"{}".format(acc[0][3]),
-				"Password":"{}".format(acc[0][4]),
-				"Birthday":"{}".format(acc[0][5])
-		}
-		print(account)
 		dbManager.close()
-		if account:
-			return account
+		if acc:
+			return acc
 		else:
 			return False
 
-access = LoginSystem().login("gjergjk71","gjergji.123")
-print(access["First Name"])
+#access = LoginSystem().login("gjergjk71","gjergji.123")
+#print(access["First Name"])

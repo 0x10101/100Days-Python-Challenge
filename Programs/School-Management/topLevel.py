3fea4f02739bb0cc1f783e45b752776e5fc067d8
gjergjk71 <gjergjk71@gmail.com>
2018-04-19 22:36:22 +0200

    Created createTitle(..) method

--- Programs/School-Management/topLevel.py
+++ Programs/School-Management/topLevel.py
@@ -5,7 +5,7 @@ class Create:
 		#self.frame = frame
 		pass
 
-	def top(self,frame,title="",w=500,h=500):
+	def top(self,frame,title,w=500,h=500):
 		top = tk.Toplevel()
 
 		ws = frame.winfo_screenwidth() # width of the screen
@@ -17,39 +17,49 @@ class Create:
 		top.geometry('%dx%d+%d+%d' % (w, h, x, y))
 		top.title(title)
 
-		xpos = 100
-		ypos = 100
 		return top
+	
+	def createTitle(self,frame,titleText,fontfamily="",fontsize=20):
+		title = tk.Label(frame,text=titleText,font=(fontfamily,fontsize))
+		title.place(x=50,y=30)
 
-	def top_labels(self,frame,labels,y_increment=50,fontsize=20,fontfamily="",xpos=100,ypos=100):
-		xpos = 100
-		ypos = 100
-		for label in labels:
-			label.configure(font=(fontfamily,fontsize))
+	def top_labels(self,frame,columns,y_increment=50,fontsize=20,fontfamily="",xpos=100,ypos=100):
+		labels = []
+		for column in columns:
+			print(column)
+			label = tk.Label(frame,text=column.title() + ":",font=(fontfamily,fontsize))
 			label.place(x=xpos,y=ypos)
 			ypos += y_increment
+			labels.append(label)
+		return labels
 
 
 
-	def top_entries(self,frame,entries,y_increment=50,xpos=200,ypos=100,width=250,height=50,fontsize=20,fontfamily=""):
-		for entry in entries:
-			entry.configure(font=(fontfamily,fontsize))
+	def top_entries(self,frame,columns,y_increment=50,xpos=200,ypos=100,width=250,height=50,fontsize=20,fontfamily=""):
+		entries = []
+		for column in columns:
+			print(column)
+			entry = tk.Entry(frame,font=(fontfamily,fontsize))
 			entry.place(x=xpos,y=ypos,width=width,height=height)
 			ypos += y_increment
+			entries.append(entry)
+		return entries
 
-"""
 root = tk.Tk()
 
 create = Create()
-top = create.top(root)
+top = create.top(root,"Add Employee")
 l1 = tk.Label(top,text="DSA")
 l2 = tk.Label(top,text="DAS")
 
 e1 = tk.Entry(top)
 e2 = tk.Entry(top)
-create.top_labels(top,[l1,l2])
-create.top_entries(top,[e1,e2])
 
+employees = "firstName,lastName,birthday,phone,address,role,classes,wage"
+
+create.createTitle(top,"Add Employee")
+labels = create.top_labels(top,str.split(employees,","))
+create.top_entries(top,str.split(employees,","),width=150,height=40,xpos=300)
+print(labels)
 root.mainloop()
-"""
 
